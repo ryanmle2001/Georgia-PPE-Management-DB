dbdd49bf4204619412c001777e238b477ed05053
Rajit Khanna <rkhanna23@gatech.edu>
2020-07-03 13:06:44 -0400

    conformed to naming conventions

--- src/rajit.sql
+++ src/rajit.sql
@@ -16,25 +16,25 @@
 
 -- CREATE TABLE STATEMENTS BELOW
 
-CREATE TABLE `business` (
-  `name` varchar(1) NOT NULL PRIMARY KEY,
-  `street` varchar(1) NOT NULL,
-  `city` varchar(1) NOT NULL,
-  `zip` decimal(5, 0) NOT NULL,
-  UNIQUE KEY (`street`, `city`, `zip`)
+CREATE TABLE BUSINESS (
+  business_name varchar(1) NOT NULL PRIMARY KEY,
+  street varchar(1) NOT NULL,
+  city varchar(1) NOT NULL,
+  zip decimal(5, 0) NOT NULL,
+  UNIQUE KEY (street, city, zip)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `hospital` (
-  `name` varchar(1) NOT NULL PRIMARY KEY,
-  `max_doctors` decimal(2,0) NOT NULL,
-  `budget` decimal(20,2) NOT NULL,
-  CONSTRAINT hospital_ibfk_1 FOREIGN KEY (name) REFERENCES business (name)
+CREATE TABLE HOSPITAL (
+  hospital_name varchar(1) NOT NULL PRIMARY KEY,
+  max_doctors decimal(2,0) NOT NULL,
+  budget decimal(20,2) NOT NULL,
+  CONSTRAINT hospital_ibfk_1 FOREIGN KEY (hospital_name) REFERENCES business (business_name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `manufacturer` (
-  `name` varchar(1) NOT NULL PRIMARY KEY,
-  `catalog_capacity` decimal(5,0) NOT NULL,
-  CONSTRAINT manufacturer_ibfk_1 FOREIGN KEY (name) REFERENCES business (name)
+CREATE TABLE MANUFACTURER (
+  manufacturer_name varchar(1) NOT NULL PRIMARY KEY,
+  catalog_capacity decimal(5,0) NOT NULL,
+  CONSTRAINT manufacturer_ibfk_1 FOREIGN KEY (manufacturer_name) REFERENCES business (business_name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
