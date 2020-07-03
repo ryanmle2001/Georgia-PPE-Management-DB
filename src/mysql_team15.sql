86fa683b1fc744c2c5ebfaf7bfc5c32d16685ddb
Rajit Khanna <rkhanna23@gatech.edu>
2020-07-03 12:46:01 -0400

    added create table statements

---
+++ src/mysql_team15.sql
@@ -0,0 +1,24 @@
+-- CS4400: Introduction to Database Systems
+-- Summer 2020
+-- Phase II Create Table and Insert Statements Template
+
+-- Team XX
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+
+-- Directions:
+-- Please follow all instructions from the Phase II assignment PDF.
+-- This file must run without error for credit.
+-- Create Table statements should be manually written, not taken from an SQL Dump.
+-- Rename file to cs4400_phase2_teamX.sql before submission
+
+-- CREATE TABLE STATEMENTS BELOW
+
+
+
+
+
+
+-- INSERT STATEMENTS BELOW
